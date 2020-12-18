1accfcc5ecc217178e680b83a45f28c1bdbc123d
joenjihwan <s20062@gsm.hs.kr>
2020-12-18 23:15:56 +0900

    [ADD]Change file.

---
+++ Griddy/Change.py
@@ -0,0 +1,13 @@
+# 가장 최소의 동전 개수로 1260원을 거슬러 주는 방법은?
+
+n = 1260 # 거스름돈
+count = 0
+
+coin_types = [500,100,50,10]
+
+for coin in coin_types:
+    count += n //coin # 해당 화폐로 거슬러 줄 수 있는 동전 개수
+    n%=coin
+    #print(n) # 260, 60, 10, 0 이 차례로 반환
+
+print(count)
