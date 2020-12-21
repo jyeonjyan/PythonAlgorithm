3197bfa5780a2d49a0da310b89c96bbbe284cc5a
joenjihwan <s20062@gsm.hs.kr>
2020-12-21 18:09:10 +0900

    [UPDATE]NumberCardEx

---
+++ Griddy/NumberCard.py
@@ -0,0 +1,13 @@
+# n,m 을 공백으로 구분하여 입력받기
+n,m = map(int, input().split())
+result=0
+
+#한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split))
+    #현재 줄에서 가장 작은 수 출력
+    min_value = min(data)
+    #가장 작은 수들 중 가장 큰 수 출력
+    result = max(result, min_value)
+    
+print(result)
