545668c4f0742837813a57433509118d74574f7c
johnjihwan <s20062@gsm.hs.kr>
2021-01-13 10:26:09 +0900

    [ADD]what is stack

---
+++ DFS/BFS/stack.py
@@ -0,0 +1,13 @@
+stack = []
+
+stack.append(5)
+stack.append(4)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(8)
+stack.append(9)
+stack.pop()
+
+print(stack) #최하단 원소부터 출력
+print(stack[::-1]) #최상단 원소부터 출력
