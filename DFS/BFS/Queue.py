6dbd266038da1f3286bfdccb4c7b04cfc05467c0
johnjihwan <s20062@gsm.hs.kr>
2021-01-13 11:33:08 +0900

    [ADD]Queue ex

---
+++ DFS/BFS/Queue.py
@@ -0,0 +1,16 @@
+from collections import deque
+#큐 구현을 위해 deque 라이브러리 사용
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse() #역순으로 출력을 위해 reverse.
+print(queue)
