6b2f8c000e6cd6cb060c812512ffdf1f8cc77adb
johnjihwan <s20062@gsm.hs.kr>
2021-02-10 13:57:24 +0900

    [ADD]How to calculate avg?

---
+++ Programmers/avg.py
@@ -0,0 +1,5 @@
+def solution(arr):
+    print(sum(arr)/len(arr))
+
+array = [1,2,3]
+solution(array)
---
+++ Programmers/hateThesameNum.py
@@ -0,0 +1,7 @@
+# def solution(arr):
+#     save = []
+#     for i in range(len(arr)):
+#         if arr[i] == arr[i+1]:
+#             save = arr[i]
+# arr = [1,1,3,3,0,1,1]
+# solution(arr)
